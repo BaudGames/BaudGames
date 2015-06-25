1c708033c3a4547ff8d32f10560bd688171f555a
TTr2 <tangoindigomacho@gmail.com>
2015-06-25 16:38:10 +0100

    25/06/2015 - Home page setup for running Blackjack script or sending login details to server.

--- index.php
+++ index.php
@@ -10,7 +10,29 @@ and open the template in the editor.
         <title>Baud Games</title>
     </head>
     <body>
+        <header>
+            <img src="images/Facebook-Banner.jpg"
+            width="851"
+            height="315"
+            alt="You know who we are."
+            >
+        </header>
+        
         <p>Welcome to Baud Games.</p>
+        <p>Don't get BORED, get <b>BAUD</b>.</p>
+        
+        <p>Want to play Blackjack?</p>
+        <form action="http://localhost/BaudGames/games/Blackjack/index.php"
+            method="post"
+            <p><input type="submit" name="play" value="PLAY!"/></p>           
+        </form>
+                
+        <form action="http://localhost/BaudGames/controllers/user_login.php" method="post">
+            <p> Name: <input type="text" name="name"/></p>
+            <p> Email: <input type="text" name="email"/></p>
+            <p> Password: <input type="text" name="password"/></p> 
+            <p><input type="submit" /></p>
+        </form>
         <?php
         // put your code here
         ?>
